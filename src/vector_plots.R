0dbb92f86925c4c143f6f473e4b1cfbf74707373
nwagenbrenner <nwagenbrenner@gmail.com>
2014-06-22 15:44:15 -0600

    vector plots faceted on forecast type (includes observed field)

--- src/vector_plots.R
+++ src/vector_plots.R
@@ -18,29 +18,61 @@ l2 <- list("NAM", "HRRR", "WRFUW", "WRFNARR") #list of forecast names
 
 data<-wnBuildBiasDf(l, l2) #build main df
 
-t<-as.POSIXct(strptime("2010-07-18 17:00:00", '%Y-%m-%d %H:%M:%S'))
+t<-as.POSIXct(strptime("2010-07-19 02:00:00", '%Y-%m-%d %H:%M:%S'))
 
 #subset on time
 s <- subset(data, subset=(datetime == t))
-#subset on forecast
-#s <-subset(s, subset=(fcastName=='WindNinja-NAM'))
-s <-subset(s, subset=(fcastName=='NAM (12 km)'))
 
-s.hr <- buildBiasHourlyAverages(s)
+#subset on forecast and build hourly averages
+ss <-subset(s, subset=(fcastName=='WindNinja-NAM'))
+s.hr <- buildBiasHourlyAverages(ss)
+s.hr$fcastName<-'WindNinja-NAM'
+ss <-subset(s, subset=(fcastName=='NAM (12 km)'))
+s.hr.temp <- buildBiasHourlyAverages(ss)
+s.hr.temp$fcastName<-'NAM (12 km)'
+s.hr<-rbind(s.hr, s.hr.temp)
+
+
+#s <-subset(s, subset=(fcastName=='WRF-NARR (1.33 km)'))
+#s <-subset(s, subset=(fcastName=='WindNinja-WRF-NARR'))
+
+
 
 #bsb
 lat = 43.402726
 lon = -113.027724
 zoom = 12
 maptype = 'terrain' #terrain, hybrid
-datatype = 'observed' #observed or predicted
-colorscale='continuous' #discrete or continous
+datatype = 'predicted' #observed or predicted
+colorscale='discrete' #discrete or continous
 
-m <- wnCreateVectorMap(s.hr, lat, lon, zoom, 
+#m <- wnCreateVectorMap(s.hr, lat, lon, zoom, 
+#                       maptype, 
+#                       colorscale=colorscale, 
+#                       axis_labels=FALSE, 
+#                       datatype=datatype)
+
+m <- wnCreatePredObsVectorMap(s.hr, lat, lon, zoom, 
                        maptype, 
                        colorscale=colorscale, 
-                       axis_labels=FALSE, 
-                       datatype=datatype)
+                       axis_labels=FALSE)
+#-----------------------------------
+# make a bubble plot of the bias
+#-----------------------------------
+
+t<-as.POSIXct(strptime("2010-07-19 05:00:00", '%Y-%m-%d %H:%M:%S'))
+
+#subset on time
+s <- subset(data, subset=(datetime == t))
+
+#model<-"WRF-NARR (1.33 km)"
+model<-"NAM (12 km)"
+
+var<-"speed"
+stat<-"bias"
+breaks<-5
+
+test<-wnCreateBubbleMap(s, model, var, stat, breaks)
 
 
 
