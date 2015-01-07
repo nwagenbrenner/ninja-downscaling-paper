089720c794bd4cfedfb688dbce0da31922873f3c
nwagenbrenner <nwagenbrenner@gmail.com>
2015-01-07 14:08:00 -0700

    new time series plots

--- src/sensor_timeseries_plots.R
+++ src/sensor_timeseries_plots.R
@@ -18,19 +18,25 @@ l2 <- list("NAM", "HRRR", "WRFUW", "WRFNARR") #list of forecast names
 
 data<-wnBuildBiasDf(l, l2) #build main df
 
-s<-subset(data, subset=(plot=='R26'))
+s<-subset(data, subset=(plot=='R2'))
 
 s$fcastTypeOrdered <- factor(s$fcastType, levels=c("Weather Model", "WindNinja"))
 
-p <- ggplot(s, aes(x=datetime, y=obs_speed)) +
+color_list <- c("darkorange", "red", "darkgreen", "darkblue")
+
+ss<-subset(s, subset=(wxType %in% c('NAM (12 km)', 'HRRR (3 km)')))
+
+#--- plot the time series ----------------------------------------------------
+p <- ggplot(ss, aes(x=datetime, y=obs_speed)) +
         geom_point(shape=19, size=1.5, alpha = 1.0, colour='black') +
         geom_line() +
+        theme_bw() +
         xlab("Time") + ylab("Speed (m/s)")
 
 p <- p + theme(axis.text.x = element_text(angle = 45))
 p <- p + theme(axis.text.x = element_text(vjust = 0.5))
-p <- p + theme(axis.text.x = element_text(color='black', size=12))
-p <- p + theme(axis.text.y = element_text(color='black', size=12))
+p <- p + theme(axis.text.x = element_text(color='black', size=16))
+p <- p + theme(axis.text.y = element_text(color='black', size=16))
     
 p<-p + scale_x_datetime(breaks=c(min(s$datetime)+12*60*60,
                    min(s$datetime)+12*3*60*60,
@@ -39,8 +45,10 @@ p<-p + scale_x_datetime(breaks=c(min(s$datetime)+12*60*60,
                    min(s$datetime)+12*9*60*60))
     
 
-p<-p + geom_point(data=s, aes(x=datetime, y=pred_speed, colour=wxType),shape = 19, size=1.5, alpha = 0.5)
-p<-p + geom_line(data=s, aes(x=datetime, y=pred_speed, colour=wxType, linetype=fcastTypeOrdered))
+p<- p + theme(axis.title=element_text(size=16))
+
+p<-p + geom_point(data=ss, aes(x=datetime, y=pred_speed, colour=wxType),shape = 19, size=1.5, alpha = 0.5)
+p<-p + geom_line(data=ss, aes(x=datetime, y=pred_speed, size = 0.0001, colour=wxType, linetype=fcastTypeOrdered))
    
 p<-p + scale_colour_manual(values=color_list, name="Model Type")
 
